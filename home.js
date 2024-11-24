300546e8aaa627a14e0b912f6788cf8c91fd1c62
AliYusifoov <ali.yusifov.09@mail.ru>
2024-11-24 21:59:07 +0400

    third3

--- home.js
+++ home.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper(".swiper", {
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  });
 $(document).ready(function () {
     $("h1").text("İstifadəçi adı: " + localStorage.getItem("username"));
     
